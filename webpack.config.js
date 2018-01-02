0973fbb7bbf5859cbe699f801db828fcaf8bb89a
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-01-02 11:54:53 -0800

    Add webpack output to assets folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
  module.exports = {
      entry: "./src/_js/app.js",
      output: {
-         path: __dirname + '/dist',
+         path: __dirname + '/src/assets',
          filename: "bundle.js"
      },
      module: {

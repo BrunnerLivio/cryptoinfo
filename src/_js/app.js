e3df4b46aa8ebefd3e9287dcc1ffefd2dd48e2d0
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-01-03 12:10:05 -0800

    Add scss style into webpack

--- src/_js/app.js
+++ src/_js/app.js
@@ -1,5 +1,6 @@
 import 'reset-css/reset.css';
 import 'flexboxgrid/dist/flexboxgrid.css';
+import '../_scss/2018/main.scss';
 
 (() => {
   console.log('Works!');
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,19 @@
              }
          }],
          rules: [{
-             test: /\.css$$/,
-             use: ['style-loader', 'css-loader']
-         }]
+                 test: /\.css$$/,
+                 use: ['style-loader', 'css-loader']
+             }, {
+                 test: /\.scss$/,
+                 use: [{
+                     loader: "style-loader"
+                 }, {
+                     loader: "css-loader"
+                 }, {
+                     loader: "sass-loader"
+                 }]
+             }
+
+         ]
      }
  };

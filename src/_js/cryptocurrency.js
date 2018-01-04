c69ef7e558168b6c1f0b77c99e02a8b16e33a2ba
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-01-04 10:42:14 +0100

    Add change and short form metadata

--- src/_js/cryptocurrency.js
+++ src/_js/cryptocurrency.js
@@ -1,19 +1,45 @@
-import request from 'browser-request';
 import numeral from 'numeral';
+import request from 'browser-request';
 
 export class Cryptocurrency {
+    constructor() {
+        this.data = {};
+    }
     onInit() {
-        this.elements = document.querySelectorAll('.crypto-price');
-        this.elements.forEach(element => this.getPrice(element));
+        document.querySelectorAll('.crypto-price').forEach(element => this.getPrice(element));
+        document.querySelectorAll('.crypto-change').forEach(element => this.getChange(element));
+    }
+    loadData(shortForm) {
+        return new Promise((resolve, reject) => {
+            if (!this.data[shortForm]) {
+                request(`https://coincap.io/page/${shortForm}`, (err, response, body) => {
+                    this.data[shortForm] = JSON.parse(body);
+                    resolve(this.data[shortForm]);
+                })
+            } else {
+                resolve(this.data[shortForm]);
+            }
+        });
     }
     getPrice(element) {
-      const currency = element.getAttribute('data-currency').toUpperCase();
-      request(`https://coincap.io/page/${currency}`, (err, response, body) => {
-        const result = JSON.parse(body);
-        element.innerHTML = 
+        const currency = element.getAttribute('data-currency').toUpperCase();
+        this.loadData(currency)
+            .then(result => {
+                element.innerHTML =
 `
-<span>${numeral(result.price_usd).format('0,0[.]00')}</span><span class="weaken">$</span>
+<span>${numeral(result.price_usd).format('0,0[.]00')}</span><span class="weaken"> $</span>
 `
-      })
+            });
+    }
+    
+    getChange(element) {
+        const currency = element.getAttribute('data-currency').toUpperCase();
+        this.loadData(currency)
+            .then(result => {
+                element.innerHTML =
+`
+<span>${numeral(result.cap24hrChange).format('0[.]00')}</span><span class="weaken"> %</span>
+`
+            });
     }
 }
